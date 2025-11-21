97d696eb19bd36e25b936f716e04e64a5b7f5e9b
IDKGuy24 <sayan2012.4@gmail.com>
2025-11-21 06:45:15 +0000

    deploy bot

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const bot = mineflayer.createBot({
   port: config.serverPort,
   username: config.botUsername,
   auth: 'offline',
-  version: 1.21,
+  version: false,
   viewDistance: config.botChunk
 });
 
